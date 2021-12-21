786a3fbc6530535ec7c03fc8b3661430b55a362c
sin lee <leesin350541732@gmail.com>
2021-12-21 09:51:12 +0800

    add fix bugs

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -20,7 +20,7 @@ import axios from 'axios'
 export default {
   name: 'Home',
   components: { HomeHeader, HomeSwiper, HomeIcons, HomeRecommend, HomeWeekend },
-  data: function () {
+  data() {
     return {
       city: '',
       swiperList: [],
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -20,7 +20,6 @@ export default {
   props: {
     recommendList: [],
   },
-  data() {},
 }
 </script>
 
